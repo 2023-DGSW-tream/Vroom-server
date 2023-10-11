e7c7ce3b082683500d3b4767676c96d8e9110349
pea06 <101560095+pea06@users.noreply.github.com>
2023-10-11 21:25:38 +0900

    config: add query dsl config

---
+++ vroom-infrastructure/src/main/kotlin/kr/hs/dgsw/vroom/global/config/QueryDslConfig.kt
@@ -0,0 +1,18 @@
+package kr.hs.dgsw.vroom.global.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Configuration
+class QueryDslConfig {
+    @PersistenceContext
+    private val entityManager: EntityManager? = null
+
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory? {
+        return JPAQueryFactory(entityManager)
+    }
+}
